4cd35f67aa832aaa50a4066f7e8e4d43e147f16c
macovedj <djmacovei@gmail.com>
2020-04-19 16:17:44 -0400

    prod build and no gatsby looks

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -9,7 +9,7 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Link style={{ color: `#9f8ec2`}} to="/">Go back to the homepage</Link>
   </Layout>
 )
 
