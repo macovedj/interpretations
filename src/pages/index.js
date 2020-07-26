c0066b79167eb54134e82a89be15f0efb4b0a226
KON9759 <daniel.macovei@kroger.com>
2020-07-26 13:26:56 -0400

    format relativity

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
     </div>
-    <a style={{ color: `#9f8ec2`}} href="https://macovedj.github.io/threeRelativity/">Special Relativity Simulator</a>
+    <p><a style={{ color: `#9f8ec2`}} href="https://macovedj.github.io/threeRelativity/">Special Relativity Simulator</a></p>
     <Link style={{ color: `#9f8ec2`}} to="/page-2/">Go to page 2</Link>
   </Layout>
 )
