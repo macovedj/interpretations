4551ce747833ded3b384b2bd15c21d31f34d76e5
KON9759 <daniel.macovei@kroger.com>
2020-07-26 13:11:44 -0400

    link to relativity

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>I interpret things here</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
     </div>
+    <a style={{ color: `#9f8ec2`}} href="https://macovedj.github.io/threeRelativity/">Special Relativity Simulator</a>
     <Link style={{ color: `#9f8ec2`}} to="/page-2/">Go to page 2</Link>
   </Layout>
 )
